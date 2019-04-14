76e73c60bab36ed2d4bbc93b15c3cff4c0cd2dae
Ilito Torquato <ilitotor@gmail.com>
2019-04-14 16:08:15 -0300

    final corrections

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 from flask import render_template
 from flask import request
-from flask import redirect
+from flask import redirect, url_for
 from datetime import datetime
 
 from flask_sqlalchemy import SQLAlchemy
@@ -14,6 +14,7 @@ project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "features.db"))
 
 app = Flask(__name__)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
 
@@ -50,6 +51,7 @@ def home():
             product_area=request.form.get("product_area"),
         )
         feature_new = Feature.query.filter_by(id_feature=id_feature).first()
+        product_priority_to_compare = feature_new.client_priority
         feature_new.title = update_feature.title
         feature_new.description = update_feature.description
         feature_new.client = update_feature.client
@@ -61,9 +63,9 @@ def home():
         # increment +1 with priority added is equal other priority with the same Client
         query = Feature.query.filter_by(client=feature_new.client).all()
         for priority in query:
-            if priority.id_feature != feature_new.id_feature and int(
-                priority.client_priority
-            ) >= int(feature_new.client_priority):
+            print(product_priority_to_compare, feature_new.client_priority)
+            if priority.id_feature != feature_new.id_feature and int(feature_new.client_priority) <= int(
+                priority.client_priority) and int(feature_new.client_priority) != int(product_priority_to_compare):
                 priority.client_priority = int(priority.client_priority) + 1
                 db.session.add(priority)
 
@@ -85,9 +87,8 @@ def home():
             # increment +1 with priority added is equal other priority with the same Client
             query = Feature.query.filter_by(client=feature.client).all()
             for priority in query:
-                if priority.id_feature != feature.id_feature and int(
-                    priority.client_priority
-                ) >= int(feature.client_priority):
+                if priority.id_feature != feature.id_feature and int(feature.client_priority) <= int(
+                    priority.client_priority):
                     priority.client_priority = int(priority.client_priority) + 1
                     db.session.add(priority)
 
